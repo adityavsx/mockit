6466ebb482f7317418b1859b8ddd7a1e77c9d4c1
aditya <adityavikramsinha19@gmail.com>
2024-06-22 15:00:14 +0530

    only left with header styling [complete today] and with dynamic data [complete tomorrow]

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,8 +4,7 @@
 export default function Header() {
     return (
         <div className="w-full border-b-1 h-16 justify-center">
-            <div className="border-1 border-black w-5/6 text-center py-4 px-16 h-full text-indigo-800 underline align-center text-4xl font-bold">
-                Mockit
+            <div className="w-5/6 text-center py-4 px-16 h-full text-indigo-800 underline align-center text-4xl font-bold">
             </div>
         </div>
     )
